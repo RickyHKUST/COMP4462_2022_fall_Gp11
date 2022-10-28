7175f2044ae911832b160d460a036b242e65216f
wcleungaj <90839737+RickyHKUST@users.noreply.github.com>
2022-10-29 02:08:04 +0800

    refactor accordion

--- public/assets/panel/panel.js
+++ public/assets/panel/panel.js
@@ -1,38 +1,33 @@
 $('#statistics').append('<p>later add graph?</p>')
 
-// $('#filter').click(function(){
-//     renderFilter()
-// })
 
-// function renderFilter(){
-    // $('#filter').empty();
-    var filtering_context = ''+
-    // '<div class="title"> Filter </div>'+
-    '<div class="context"> Select which kinds of data you would like to filter </context>'+
-    '<br>'+
-        '<div class="box">'+
-            '<input type="checkbox" class="checkbox" id="housing" value="Housing">'+
-            '<label for="Housing"> Housing </label>'+
-        '</div>'+
-        '<div class="box">'+
-            '<input type="checkbox" class="checkbox" id="transportation" value="Transportation>'+
-            '<label for="Transportation"> Transportation </label>'+
-        '</div>'
-    $('#filter').append(filtering_context);
-// }
+$(".accordion").click(function(e){
+    e.target.classList.toggle("active");
+    e.target.nextElementSibling.classList.toggle("active");
+})
 
+var filtering_context = ''+
+'<div class="context"> Select which kinds of data you would like to filter </context>'+
+'<br>'+
+    '<div class="box">'+
+        '<input type="checkbox" class="checkbox" id="housing" value="Housing">'+
+        '<label for="Housing"> Housing </label>'+
+    '</div>'+
+    '<div class="box">'+
+        '<input type="checkbox" class="checkbox" id="transportation" value="Transportation>'+
+        '<label for="Transportation"> Transportation </label>'+
+    '</div>'
+$('#filter').append(filtering_context);
 
 /*
 terms:
 refactor - optimization
 
 TO DO:
-Two Accordions
-    - One for filtering
-    - One for statistics
-    - Hints: search bootstrap
-Details in Filter
-    - Transportation
-    - Hosuing
-Good styling of the whole panel
+Accordions Styling
+According lab6, show some statistics under the 'div#statistics'
+    mock some data
+    - scatter plot
+    - histogram
+Study the refactored code
 */
