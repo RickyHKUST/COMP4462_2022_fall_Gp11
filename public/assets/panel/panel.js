b0ba04610146a4f722fb77a7d954cdcf4d3a4454
smellytofuxd <paulmapaulma@gmail.com>
2022-11-12 23:30:08 +0800

    panel.js accordion refactoring & file management

--- public/assets/panel/panel.js
+++ public/assets/panel/panel.js
@@ -4,6 +4,12 @@ $('#statistics').append('<p>later add graph?</p>')
 $(".accordion").click(function(e){
     e.target.classList.toggle("active");
     e.target.nextElementSibling.classList.toggle("active");
+    if (e.target.nextElementSibling.style.display === "block") {
+        e.target.nextElementSibling.style.display = "none";
+    } 
+    else {
+        e.target.nextElementSibling.style.display = "block";
+    }
 })
 
 var filtering_context = ''+
@@ -54,47 +60,3 @@ const myChart = new Chart(ctx, {
         }
     }
 });
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
-var optionForDistrict = [
-    {value:"Hong Kong Islands",name:"Islands"},
-    {value:"Hong Kong Kwai Tsing",name:"Kwai Tsing"},
-    {value:"Hong Kong North",name:"North"},
-    {value:"Hong Kong Sai Kung",name:"Sai Kung"},
-    {value:"Hong Kong Sha Tin",name:"Sha Tin"},
-    {value:"Hong Kong Tai Po",name:"Tai Po"},
-    {value:"Hong Kong Tsuen Wan",name:"Tsuen Wan"},
-    {value:"Hong Kong Tuen Mun",name:"Tuen Mun"},
-    {value:"Hong Kong Yuen Long",name:"Yuen Long"},
-    {value:"Hong Kong Kowloon City",name:"Kowloon City"},
-    {value:"Hong Kong Kwun Tong",name:"Kwun Tong"},
-    {value:"Hong Kong Sham Shui Po",name:"Sham Shui Po"},
-    {value:"Hong Kong Wong Tai Sin",name:"Wong Tai Sin"},
-    {value:"Hong Kong Yau Tsim Mong",name:"Yau Tsim Mong"},
-    {value:"Hong Kong Central and Western",name:"Central and Western"},
-    {value:"Hong Kong Eastern",name:"Eastern"},
-    {value:"Hong Kong Southern",name:"Southern"},
-    {value:"Hong Kong Wan Chai",name:"Wan Chai"},
-]
-
-optionForDistrict.forEach(district => {
-    $('#place-names').append($('<option>', {
-        value: district.value,
-        text: district.name
-    }));
-})
-
