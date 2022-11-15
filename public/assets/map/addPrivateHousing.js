1d803d5b8960a2e62c9607099398841b9c8d9054
ivanf-09 <ivanfan09@gmail.com>
2022-11-15 21:35:24 +0800

    able to display all private housing but some lat long is not accurate, may fix later

--- public/assets/map/addPrivateHousing.js
+++ public/assets/map/addPrivateHousing.js
@@ -1,62 +1,41 @@
 let privatehousingloc = [];
 let privatemarkers = [];
-const APIkey = "AIzaSyBr30gUlrpF5Qwhr4t0R_IrqmqKxHLB1IU";
-const geocodeurl = "https://maps.googleapis.com/maps/api/geocode/json?address=";
 
 let createprivatehousing = () => {
-    let start = 1;
-    let end = 95
+    d3.csv("assets/data/housing/private.csv", function (data) {
 
-    fetch("assets/data/housing/private.json")
-        .then(response => response.json())
-        .then(data => {
+        data.forEach(privatehousing => {
+            let latitude = privatehousing.lat;
+            let longitude = privatehousing.lon;
+            let name = privatehousing.original_Name_of_Building;
 
-            for (start; start < end; start++) {
-                let address = data[start]["Address"].replace(/\s/g, '+');
-                let addressurl = geocodeurl + address + "&key=" + APIkey;
-                let name = data[start]["Name of Building"];
+            privatehousingloc.push({
+                position: new google.maps.LatLng(latitude, longitude),
+                title: name,
+                type: "private housing"
+            });
+        });
 
-                fetch(addressurl)
-                    .then(response => response.json())
-                    .then(data => {
+        const icon = {
+            url: "https://maps.google.com/mapfiles/kml/pal3/icon21.png",
+            scaledSize: new google.maps.Size(10, 10),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(0, 0)
+        };
 
-                        let latitude = data.results[0].geometry.location.lat;
-                        let longitude = data.results[0].geometry.location.lng;
+        privatemarkers.forEach(data => data.setMap(null));
 
-                        privatehousingloc.push({
-                            position: new google.maps.LatLng(latitude, longitude),
-                            title: name,
-                            type: "private housing"
-                        });
+        privatehousingloc.forEach(location => {
+            marker = new google.maps.Marker({
+                position: location.position,
+                icon: icon,
+                map: map,
+                title: location.title
+            });
 
-                        const icon = {
-                            url: "https://maps.google.com/mapfiles/kml/pal3/icon21.png",
-                            scaledSize: new google.maps.Size(10, 10),
-                            origin: new google.maps.Point(0, 0),
-                            anchor: new google.maps.Point(0, 0)
-                        };
-                        privatemarkers.forEach(data => data.setMap(null));
-
-                        privatehousingloc.forEach(location => {
-                            marker = new google.maps.Marker({
-                                position: location.position,
-                                icon: icon,
-                                map: map,
-                                title: location.title
-                            });
-
-                            privatemarkers.push(marker);
-                        })
-                    })
-
-                // if (start == end-1 && end < Object.keys(data).length) {
-                //     start = end;
-                //     end += end
-                // }
-            }
-        })
-
-        .catch (err => console.log(err))
+            privatemarkers.push(marker);
+        });
+    })
 }
 
 $("#targetMonth")[0].addEventListener("change", createprivatehousing);
