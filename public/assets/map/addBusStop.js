0df6bcf0722e98828c26be1a209579b09e53a998
ivanf-09 <ivanfan09@gmail.com>
2022-11-18 12:26:12 +0800

    trying on showing all nearby transport

--- public/assets/map/addBusStop.js
+++ public/assets/map/addBusStop.js
@@ -2,58 +2,80 @@
 proj4.defs('EPSG:2326', '+proj=tmerc +lat_0=22.31213333333334 +lon_0=114.1785555555556 +k=1 +x_0=836694.05 +y_0=819069.8 +ellps=intl +towgs84=-162.619,-276.959,-161.764,0.067753,-2.24365,-1.15883,-1.09425 +units=m +no_defs');
 
 //After a time is selected, import corresponding csv by d3 and display the bus stop on map
-var busStopLocation = []; //an array to store the bus stop location (latitude, longitude)
 var markers = []; // to manipulate the markers after created
+//clear this array when option changed
+var busStopLocation = []; //an array to store the bus stop location (latitude, longitude)
+var minibusStopLocation = [];
 
 let renderBusStop = () => {
 
-	//use d3 to read the csv according to the name of selected date
-	d3.csv("assets/data/busStop/" + $("#targetMonth")[0].value + ".csv", function (data) {
-		
-		//Now you can use 'data' variable as an array of objects
+	busStopLocation = []; //clear this array when option changed
+	markers.forEach(data => data.setMap(null));
 
-		/*There are many row, each contains a XY coordinate.
-		 However, this XY is using HK1980 Grid coordinate(not supported by google), 
-		 so here we convert it to WGS84 degree
-		 After that we use an array to store the result
-		*/
-		data.forEach(busStop => {
-			[longitude, latitude] = proj4('EPSG:2326', 'EPSG:4326', [parseInt(busStop.X), parseInt(busStop.Y)]);
+	//get and store the values of the checkboxs selected 
+	$.each($("input[name='selectTypesBus']:checked"), function () {
 
-			//push the objects one by one
-			busStopLocation.push({
-				position: new google.maps.LatLng(latitude, longitude),
-				type: "bus",
-			});
-		})
+		//use d3 to read the csv according to the name of selected date
+		d3.csv("assets/data/" + $(this).val() + "/" + $("#targetMonth")[0].value + ".csv", function (data) {
+			//Now you can use 'data' variable as an array of objects
 
-		// Create the icon of the markers.
-		// some icon provided by google: http://kml4earth.appspot.com/icons.html
-		var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';  //can use or not use
+			/*There are many row, each contains a XY coordinate.
+			 However, this XY is using HK1980 Grid coordinate(not supported by google), 
+			 so here we convert it to WGS84 degree
+			 After that we use an array to store the result
+			*/
+			data.forEach(busStop => {
+				[longitude, latitude] = proj4('EPSG:2326', 'EPSG:4326', [parseInt(busStop.X), parseInt(busStop.Y)]);
 
-		const icon = {
-			url: "assets/map/icons/default-marker.png", // url (local icon)
-			scaledSize: new google.maps.Size(5, 5), // scaled size
-			origin: new google.maps.Point(0, 0), // origin
-			anchor: new google.maps.Point(0, 0) // anchor
-		};
+				//push the objects one by one
+				busStopLocation.push({
+					position: new google.maps.LatLng(latitude, longitude),
+					type: "bus",
+				});
+			})
 
-		markers.forEach(data => data.setMap(null));
+			if ($("#bus").prop('checked') && $("#minibus").prop('checked') && busStopLocation) {
+				// if ($("input[name='selectTypesBus']:checked")[1].value == "miniBusStop") {
+					busStopLocation = 
+					minibusStopLocation = busStopLocation.slice(-data.length)
+				// }
+			}
+			
+			console.log(busStopLocation);
+			console.log(minibusStopLocation);
 
-		//add marker to the array of busStopLocation
-		busStopLocation.forEach(location =>{
-			marker = new google.maps.Marker({
-				position: location.position,
-				//icon: iconBase + 'parking_lot_maps.png',
-				icon: icon,
-				map: map,
-			});
-			markers.push(marker);//store the marker for next time renew (see setMap(null)); otherwise it will exist forever
-		}) //end of for loop
+			// Create the icon of the markers.
+			// some icon provided by google: http://kml4earth.appspot.com/icons.html
+			var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';  //can use or not use
+
+			const icon = {
+				url: "assets/map/icons/default-marker.png", // url (local icon)
+				scaledSize: new google.maps.Size(5, 5), // scaled size
+				origin: new google.maps.Point(0, 0), // origin
+				anchor: new google.maps.Point(0, 0) // anchor
+			};
+
+
+
+			//add marker to the array of busStopLocation
+			busStopLocation.forEach(location => {
+				marker = new google.maps.Marker({
+					position: location.position,
+					//icon: iconBase + 'parking_lot_maps.png',
+					icon: icon,
+					map: map,
+				});
+				markers.push(marker);//store the marker for next time renew (see setMap(null)); otherwise it will exist forever
+			}) //end of for loop
+
+		});
 
 	});
 
 }
 
 $("#targetMonth")[0].addEventListener("change", renderBusStop);
-renderBusStop()
+
+$('input[name=selectTypesBus]').change(function () {
+	renderBusStop();
+});
--- public/assets/map/showInfoWindow.js
+++ public/assets/map/showInfoWindow.js
@@ -1,5 +1,8 @@
 let buscount = 0;
-let meter = 0;
+let minibuscount = 0;
+let lightrailcount = 0;
+let mtrcount = 0;
+let transport = [];
 
 let infowindowarr = [];
 
@@ -14,6 +17,7 @@ let showInfoWindow = (marker, name) => {
             infowindowarr.shift();
         }
 
+        let infowindow = new google.maps.InfoWindow()
         let circle = new google.maps.Circle({
             strokeColor: "#00FF00",
             strokeOpacity: 0.8,
@@ -22,19 +26,18 @@ let showInfoWindow = (marker, name) => {
             fillOpacity: 0.35,
             map,
             center: {"lat": marker.position.lat(), "lng": marker.position.lng()},
-            radius: parseInt(meter),
+            radius: parseInt($("#slider")[0].value),
         })
 
-        let infowindow = new google.maps.InfoWindow()
-        showTotalBusStops(marker);
+        showTotalStops(marker);
         infowindow.setContent(
             "<h3>" + name + "</h3>" +
             "<p>Nearby Bus Stops (" + meter + "m): " + buscount + "</p>" +
-            "<p>Nearby Minibus Stops (" + meter + "m): " + "count" + "</p>" +
-            "<p>Nearby Lightrail Station (" + meter + "m): " + "count" + "</p>" +
-            "<p>Nearby MTR Stations (" + meter + "m): " + "count" + "</p>"
+            "<p>Nearby Minibus Stops (" + meter + "m): " + minibuscount + "</p>" +
+            "<p>Nearby MTR Stations (" + meter + "m): " + mtrcount + "</p>" +
+            "<p>Nearby Lightrail Station (" + meter + "m): " + lightrailcount + "</p>"
         )
-        
+
         infowindow.open(marker.map, marker)
         setTimeout(() => {
             infowindow.close();
@@ -45,47 +48,106 @@ let showInfoWindow = (marker, name) => {
     })
 }
 
-let showTotalBusStops = (house_marker) => {
+let showTotalStops = (house_marker) => {
     let slider = $("#slider");
     let output = $("#m_value");
 
-    let house_lat = house_marker.position.lat()
-    let house_lng = house_marker.position.lng()
+    let house_lat = house_marker.position.lat();
+    let house_lng = house_marker.position.lng();
 
-    //dynamic?
+    let busMarkers = [];
+    let minibusMarkers = [];
+    let transport = [lighRailMarkers, mtrMarkers, busStopLocation, minibusStopLocation]
+    console.log(busStopLocation);
+    console.log(minibusStopLocation);
+    // if (!transport.includes((lighRailMarkers, mtrMarkers))) {transport.push(lighRailMarkers, mtrMarkers)}
+
+    meter = $("#slider")[0].value;
+
+    // for (let i in $("input[name='selectTypesBus']:checked")){
+    //     switch ($("input[name='selectTypesBus']:checked")[i].value) {
+    //     case "busStop":
+    //         if (transport.length == 3) {
+    //             let temp = transport[2];
+    //             if (transport[2].length > 5000) {
+    //                 transport.push(transport[2].filter((item, index), transport[2].indexOf(item) !== index));
+    //                 transport[2] = transport[2].filter((item, index), transport[2].indexOf(item) === index);
+    //             }
+    //         }   
+    //         else if (transport.length == 4) {
+    //                 if (transport[2] == transport[3]) {
+    //                     transport[2] = transport[2].filter((item, index), transport[2].indexOf(item) === index);
+    //                 }
+    //         }
+            
+    //         else {
+    //             busMarkers = markers
+    //             transport.push(busMarkers)
+    //             console.log(transport)
+    //         }
+    //         break;
+    //     case "miniBusStop":
+    //         console.log("MBUS")
+    //         minibusMarkers = markers
+    //         console.log(busMarkers)
+    //         if (busMarkers.length != 0) {
+    //             console.log("in");
+    //             minibusMarkers = minibusMarkers.filter(x => busMarkers.indexOf(x) == -1)
+    //         }
+    //         transport.push(minibusMarkers)
+    //         console.log("iner mbus") 
+    //         console.log(minibusMarkers)
+    //         console.log(transport);
+    //         break;
+    //     }
+    // }
+
+    transport.forEach(t => {
+        count = counting(house_lat, house_lng, meter, t);
+
+        switch (t) {
+            case busStopLocation:
+                buscount = count;
+                break;
+            case minibusStopLocation:
+                minibuscount = count;
+                break;
+            case lighRailMarkers:
+                lightrailcount = count;
+                break;
+            case mtrMarkers:
+                mtrcount = count;
+                break;
+        }
+    })
 
     slider
-    .ready(function() {
-        meter = slider[0].value;
-        counting(house_lat, house_lng, meter)
-    })
     .on("input", function() {
         output.html(slider[0].value)
     })
     .on("change", function() { 
         meter = slider[0].value;
-        counting(house_lat, house_lng, meter);
     })
-
 }
 
 let counting = (house_lat, house_lng, meter, transportloc) => {
-    
-    buscount = 0;
 
-    busStopLocation.forEach(element => {
+    count = 0;
+
+    transportloc.forEach(element => {
         //get every stop's location
-        let bus_lat = element.position.lat()
-        let bus_lng = element.position.lng();
+        let lat = element.position.lat()
+        let lng = element.position.lng();
 
         //check if the stop(s) is/are between the area
         let ky = 40000 / 360;
         let kx = Math.cos(Math.PI * house_lat / 180.0) * ky;
-        let dx = Math.abs(house_lng - bus_lng) * kx;
-        let dy = Math.abs(house_lat - bus_lat) * ky;
+        let dx = Math.abs(house_lng - lng) * kx;
+        let dy = Math.abs(house_lat - lat) * ky;
 
         let result = Math.sqrt(dx * dx + dy * dy) <= (meter/1000)
 
-        if (result) {++buscount;}
+        if (result) {++count;}
     })
+    return count;
 }
