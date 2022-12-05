bfac8eaab31dbbb4dcb3463a2065c8fde945037b
smellytofuxd <paulmapaulma@gmail.com>
2022-12-05 15:17:46 +0800

    default options: index.html->panel.js

--- public/assets/panel/panel.js
+++ public/assets/panel/panel.js
@@ -1,34 +1,5 @@
-var busAndMtrCheckbox = [
-    {input:'<input type="checkbox" id="bus" name="selectTypesBus" value="busStop">',label:'<label for="bus">Bus Stops</label>'},
-    {input:'<input type="checkbox" id="minibus" name="selectTypesBus" value="miniBusStop">',label:'<label for="minibus">Minibus Stops</label>'},
-    {input:'<input type="checkbox" id="mtr" name="selectTypesMtr" value="mtr">',label:'<label for="mtr">MTR Stations</label>'},
-    {input:'<input type="checkbox" id="lightRail" name="selectTypeslightRail" value="lightRail">',label:'<label for="lightRail">Light Rail Stations</label>'},
-    {input:'<input type="checkbox" id="public" name="publichousing" value="publichousing">',label:'<label for="public">Public Housing</label>'},
-    {input:'<input type="checkbox" id="private" name="sprivatehousing" value="publichousing">',label:'<label for="private">Private Housing</label>'}
-]
-
-var optionForPlotX = [
-    {value:"NearestBus",name:"Nearest bus stop distance","type":"quantitative"},
-    {value:"NearestMinibus",name:"Nearest minibus stop distance","type":"quantitative"},
-    {value:"NearestMTR",name:"Nearest MTR station distance","type":"quantitative"},
-    {value:"district",name:"District","type":"qualitative"},
-    {value:"MTRDistance",name:"1/2/3/4/5km to MTR","type":"qualitative"}
-]
-
-var optionForPlotY = [
-    {value:"NearestBus",name:"Nearest bus stop distance","type":"quantitative"},
-    {value:"NearestMinibus",name:"Nearest minibus stop distance","type":"quantitative"},
-    {value:"NearestMTR",name:"Nearest MTR station distance","type":"quantitative"},
-    {value:"BuildingNumber",name:"Number of Building","type":"quantitative"},
-    {value:"BuildingNumberPerKm2",name:"Number of Building per km2","type":"quantitative"}
-]
-
-var optionForPlotColor = [
-    {value: "", name:"Default"},
-    {value: "by_district", name:"By district"}
-]
-
 var optionForDistrict = [
+    {name:"(Select district)"},
     {value:"Hong Kong",name:"Hong Kong"},
     {value:"Hong Kong Islands",name:"Islands"},
     {value:"Hong Kong Kwai Tsing",name:"Kwai Tsing"},
@@ -50,7 +21,41 @@ var optionForDistrict = [
     {value:"Hong Kong Wan Chai",name:"Wan Chai"},
 ]
 
+var busAndMtrCheckbox = [
+    {input:'<input type="checkbox" id="bus" name="selectTypesBus" value="busStop">',label:'<label for="bus">Bus Stops</label>'},
+    {input:'<input type="checkbox" id="minibus" name="selectTypesBus" value="miniBusStop">',label:'<label for="minibus">Minibus Stops</label>'},
+    {input:'<input type="checkbox" id="mtr" name="selectTypesMtr" value="mtr">',label:'<label for="mtr">MTR Stations</label>'},
+    {input:'<input type="checkbox" id="lightRail" name="selectTypeslightRail" value="lightRail">',label:'<label for="lightRail">Light Rail Stations</label>'},
+    {input:'<input type="checkbox" id="public" name="publichousing" value="publichousing">',label:'<label for="public">Public Housing</label>'},
+    {input:'<input type="checkbox" id="private" name="sprivatehousing" value="publichousing">',label:'<label for="private">Private Housing</label>'}
+]
+
+var optionForPlotX = [
+    {name:"(Select x-axis)"},
+    {value:"NearestBus",name:"Nearest bus stop distance","type":"quantitative"},
+    {value:"NearestMinibus",name:"Nearest minibus stop distance","type":"quantitative"},
+    {value:"NearestMTR",name:"Nearest MTR station distance","type":"quantitative"},
+    {value:"district",name:"District","type":"qualitative"},
+    {value:"MTRDistance",name:"1/2/3/4/5km to MTR","type":"qualitative"}
+]
+
+var optionForPlotY = [
+    {name:"(Select y-axis)"},
+    {value:"NearestBus",name:"Nearest bus stop distance","type":"quantitative"},
+    {value:"NearestMinibus",name:"Nearest minibus stop distance","type":"quantitative"},
+    {value:"NearestMTR",name:"Nearest MTR station distance","type":"quantitative"},
+    {value:"BuildingNumber",name:"Number of Building","type":"quantitative"},
+    {value:"BuildingNumberPerKm2",name:"Number of Building per km2","type":"quantitative"}
+]
+
+var optionForPlotColor = [
+    {name:"(Select color)"},
+    {value: "", name:"Default"},
+    {value: "by_district", name:"By district"}
+]
+
 var optionForPlotDistrict = [
+    {name:"(Select specific district)"},
     {value: "", name:"Default"},
     {value: "Central and Western", name:"Central and Western"},
     {value: "Islands", name:"Islands"},
